076040c5bccfc8d41aaf52db77ab72b56125d2d1
Lengnius <jkbentaylor@126.com>
2018-01-24 00:34:44 +0800

    orm model needs to be complete

--- www/orm.py
+++ www/orm.py
@@ -54,6 +54,96 @@ def execute(sql, args):
             raise e
         return affected
 
+def create_args_string(num):
+    L = []
+    for n in range(num):
+        L.append('?')
+    return ', '.join(L)
+
+
+class Field(object):
+    """user fields"""
+    def __init__(self, name, column_type, primary_key, default):
+        self.arg = arg
+        self.name = name
+        self.column_type = column_type
+        self.default = default
+
+    def __str__(self):
+        return '<%s, %s:%s>' % (self.__class__.__name__, self.column_type, self.name)
+
+class StringField(Field):
+    """to reflect varchar"""
+    def __init__(self, name=None, primary_key=False, default=None, ddl='varchar(100)'):
+        super().__init__(name, dll, primary_key, default)
+
+class BooleanField(Field):
+
+    def __init__(self, name=None, default=False):
+        super().__init__(name, 'boolean', False, default)
+
+class IntegerField(Field):
+
+    def __init__(self, name=None, primary_key=False, default=0):
+        super().__init__(name, 'bigint', primary_key, default)
+
+class FloatField(Field):
+
+    def __init__(self, name=None, primary_key=False, default=0.0):
+        super().__init__(name, 'real', primary_key, default)
+
+class TextField(Field):
+
+    def __init__(self, name=None, default=None):
+        super().__init__(name, 'text', False, default)
+
+class ModelMetaclass(type):
+    """docstring for ModelMetaclass"""
+    def __new__(cls, name, bases, attrs):
+        if name=='Model':
+            return type.__new__(cls, name, bases, attrs)
+
+        tableName = attrs.get('__name__', None) or name
+        logging.info('found model:%s, table:%s' %(name, tableName))
+
+        mappings = dict()
+        fields = []
+        primaryKey = None
+        for k, v in attrs.items():
+            logging.info('  found mapping: %s ==> %s' % (k, v))
+            mappings[k] = v
+            if v.primary_key:
+                
+                if primaryKey:
+                    raise RuntimeError('Duplicate primary key for fields:%s' % k)
+                    
+                primaryKey = k
+            else:
+                fields.append(k)
+
+        if not primaryKey:
+            raise RuntimeError('Primary key not found.')
+        for k in mappings.keys():
+            attrs.pop(k)
+
+        escaped_fields = list(map(lambda f: '`%s`' %f, fields))
+        attrs['__mappings__'] = mappings
+        attrs['__table__'] = tableName
+        attrs['__primary_key__'] = primaryKey
+        attrs['__fields__'] = fields
+
+        attrs['__select__'] = 'select `%s`, %s from `%s`' % (primaryKey, ','.join(escaped_fields), tableName)
+        attrs['__insert__'] = 'insert into `%s` (%s, `%s`) values (%s)' 
+        %(tableName, ','.join(escaped_fields),
+        primaryKey,
+        create_args_string(len(escaped_fields)+1))
+
+        attrs['__update__'] = 'update `%s` set %s where `%s`=?' 
+        %(tableName, ','.join(map(lambda f: '`%s`=?' % (mappings.get(f).name or f ),
+            fields)),primaryKey)
+        attrs['__delete__'] = 'delete from `%s` where `%s`=?' % (tableName, primaryKey)
+        return tyep.__new__(cls, name, bases, attrs)
+
 class Model(dict, metaclass=ModelMetaclass):
     """basic class for a object to model
 
@@ -83,24 +173,22 @@ class Model(dict, metaclass=ModelMetaclass):
                 setattr(self, key, value)
         return value
 
+    @classmethod
+    @asyncio.coroutine
+    def find(cls, pk):
+        'find object by primary key.'
+        rs = yield from select('%s where `%s`=?' % (cls.__select__, cls.__primary_key__), [pk], 1)
+        if len(rs)==0:
+            return None
+        return cls(**rs[0])
 
-class Field(object):
-    """user fields"""
-    def __init__(self, name, column_type, primary_key, default):
-        self.arg = arg
-        self.name = name
-        self.column_type = column_type
-        self.default = default
-
-    def __str__(self):
-        return '<%s, %s:%s>' % (self.__class__.__name__, self.column_type, self.name)
-
-class StringField(Field):
-    """to reflect varchar"""
-    def __init__(self, name=None, primary_key=False, default=None, ddl='varchar(100)'):
-        super().__init__(name, dll, primary_key, default)
-        
-        
+    @asyncio.coroutine
+    def save(self):
+        args = list(map(self.getValueOrDefault, self.__fields__))
+        args.append(self.getValueOrDefault(self.__primary_key__))
+        rows = yield from execute(self.__insert__, args)
+        if rows != 1:
+            logging.warn('fail to insert record: affected rows %s' % rows)
+        pass
 
 
-        
